be076d7b6d403631177823beafda39741ca7a5b0
luiz.junior.ext <luiz.junior.ext@panonline.com.br>
2024-06-05 19:33:18 -0300

    adding ios code

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,50 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
-
-	var body: some View {
-		Text(greet)
-	}
+    @StateObject
+    var viewModel = HomeViewModel()
+    
+    var body: some View {
+        VStack {
+            if viewModel.response?.isSuccess() == true {
+                List(viewModel.response?.getProducts().items ?? [], id: \.id) { element in
+                    ProductView(product: element)
+                }
+            } else if viewModel.response?.isError() == true {
+                VStack {
+                    Spacer()
+                    Text("\(viewModel.response?.getErrorMessage() ?? "Erro desconhecido")").fontWeight(.bold)
+                    Spacer()
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            } else if viewModel.response?.isLoading() == true {
+                VStack {
+                    Spacer()
+                    ProgressView("Loading")
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .padding()
+                    Spacer()
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }.task {
+            await viewModel.fetchData()
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+class HomeViewModel: ObservableObject {
+    @Published
+    private(set) var response: RequestState? = nil
+    
+    @MainActor
+    func fetchData() async {
+        for await requestState in ProductsApi().fetchProducts(limit: 10) {
+            response = requestState
+        }
+    }
 }
